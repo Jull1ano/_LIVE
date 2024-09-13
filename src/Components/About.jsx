50390edfeaee3b9b7d4c7c21dce74d3cf68d80b6
Jull1ano <iulian181@gmail.com>
2024-09-13 20:51:59 +0300

    story 02

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -13,38 +13,38 @@ export function About() {
 
                         <div className="statement">
                             <div className="bigtitle">
-                                Imagine a world where technology doesn't just function, but <span>enchants</span>.
+                                Design as a <span>Human Connection</span>
                             </div>
                             <div className="paratext">
-                                Design has always been my way of connecting with people. From the early days of my career, I understood that great design isn’t just about aesthetics—it’s about empathy, problem-solving, and creating experiences that make a real difference in people’s lives. This mindset has shaped every project I’ve led, from redesigning websites to crafting complex design systems.
+                                Design has always been my way of <span>connecting with people</span>. From the early days of my career, I understood that great design isn’t just about aesthetics—it’s about <span>empathy, problem-solving</span>, and creating experiences that make a real difference in people’s lives. This mindset has shaped every project I’ve led, from redesigning websites to crafting complex <span> design systems</span>.
                             </div>
                         </div>
 
 
                         <div className="statement">
                             <div className="bigtitle">
-                                My journey began with a <span>storyteller's heart</span>.
+                                Merging Technology with <span>Human-Centered</span> Storytelling
                             </div>
                             <div className="paratext">
-                                My Master's in Multimedia and Audio-Visual Production from the University of Bucharest wasn't just about technology; it was about understanding people and crafting narratives that resonate. This foundation honed my eye for detail and instilled a deep-seated empathy for user needs – skills that translate beautifully into user-centered design.
+                                My Master's in <span>Multimedia and Audio-Visual Production</span> from the University of Bucharest wasn't just about technology; it was about <span>understanding people</span> and crafting narratives that resonate. This foundation sharpened my attention to detail and instilled a deep-seated empathy for <span>user needs</span> – skills that translate beautifully into user-centered design.
                             </div>
                         </div>
 
                         <div className="statement">
                             <div className="bigtitle">
-                                My <span>professional</span> path took a defining turn at Tradeville.
+                                Redesigning for Impact: Blending <span>Creativity</span> and Technical Precision
                             </div>
                             <div className="paratext">
-                                As a UI/UX Designer, I spearheaded the redesign of their entire website. This wasn't just aesthetics; it was about crafting a more intuitive and engaging experience. I meticulously crafted high-fidelity designs, ensuring every element aligned with user expectations. Translating these designs into functional realities using HTML/CSS in ASP.NET showcased my technical skill and ability to bridge the gap between creativity and practicality.
+                                My professional path took a defining turn at Tradeville. As a UI/UX Designer, I took the lead in redesigning their entire website. This wasn't just aesthetics; it was about crafting a <span>more intuitive and engaging experience</span>. I meticulously crafted high-fidelity designs, ensuring every element aligned with user expectations. Translating these designs into functional realities using <span>HTML/CSS</span> in ASP.NET showcased my technical skill and ability to bridge the gap between creativity and practicality.
                             </div>
                         </div>
 
                         <div className="statement">
                             <div className="bigtitle">
-                                In 2015, I broadened my perspective by transitioning to <span>Front-End Developer</span> at Starbyte.
+                                From Design to Development: Strengthening <span>User Experiences</span> Through Code
                             </div>
                             <div className="paratext">
-                                Here, I developed fully functional websites with a mobile-first approach. This experience drilled home the importance of clean, efficient code for a seamless user experience. My ability to translate designs from Photoshop to HTML/CSS while maintaining UX/UI principles became a crucial skill I carried forward.
+                                In 2015, I broadened my perspective by transitioning to <span>Front-End Developer</span> at Starbyte. Here, I developed fully functional websites with a <span>mobile-first</span> approach. This experience drilled home the importance of clean, efficient code for a seamless user experience. My ability to translate designs from Photoshop to HTML/CSS while maintaining <span>UX/UI principles</span> became a crucial skill I carried forward.
                             </div>
                         </div>
 
@@ -53,16 +53,16 @@ export function About() {
                                 My most impactful chapter unfolded at Veeam Software, where I served as a <span>Senior Product Designer</span> for eight years.
                             </div>
                             <div className="paratext">
-                                Leading a team of seven talented designers on landing page projects, my role was not just to design but to drive meaningful business results. One of my proudest achievements was crafting <span>over 100 landing pages annually</span> for marketing campaigns. These pages, coupled with newsletters and banners, generated exceptional leads across diverse channels. I designed key visual concepts for over ten corporate websites (VeeamON, Modern Data Protection) – each demanding a keen understanding of composition, imagery, color, and typography.
+                                <span>Leading a team</span> of seven talented designers on landing page projects, my role was not just to design but to drive meaningful business results. One of my proudest achievements was crafting <span>over 100 landing pages annually</span> for marketing campaigns. These pages, coupled with newsletters and banners, generated <span>exceptional leads</span> across diverse channels. I designed <span>key visual</span> concepts for over ten corporate websites (VeeamON, Modern Data Protection) – each demanding a keen understanding of composition, imagery, color, and typography.
                             </div>
                         </div>
 
                         <div className="statement">
                             <div className="bigtitle">
-                                One of my proudest accomplishments was building a <span>Scalable Design System</span> that increased production efficiency and ensured brand consistency across all platforms.
+                                One of my proudest accomplishments was building a <span>Scalable Design System</span> that increased production efficiency.
                             </div>
                             <div className="paratext">
-                                At a pivotal moment in Veeam’s growth journey, I had the opportunity to lead the charge in establishing an <span>Efficient Design System</span>. What began as an effort to streamline our design process quickly evolved into something far more impactful. By creating a system that ensured consistency and elevated brand awareness, our team was able to produce higher-quality designs in record time. Using Figma, I crafted both low and high-fidelity prototypes, responsive designs, and reusable components that bridged the gap between concept and execution. The result? Not only enhanced the speed and quality of production but also allowed Veeam to <span>expand its global presence</span> with confidence.
+                                At a pivotal moment in Veeam’s growth journey, I had the opportunity to play a key role in establishing an <span>Efficient Design System</span>. What began as an effort to streamline our design process quickly evolved into something far more impactful. By creating a system that ensured consistency and elevated brand awareness, our team was able to produce <span>higher-quality designs in record time.</span> Using Figma, I crafted both low and high-fidelity prototypes, responsive designs, and reusable components that bridged the gap between concept and execution. The result? Not only enhanced the speed and quality of production but also allowed Veeam to <span>expand its global presence</span> with confidence.
                             </div>
                         </div>
 
@@ -71,7 +71,7 @@ export function About() {
                                 Beyond the day-to-day, I championed <span>collaboration</span>.
                             </div>
                             <div className="paratext">
-                                In a fast-paced environment where collaboration was crucial, my ability to communicate effectively and adapt to cross-functional goals became essential in driving success. When it came time to integrate Figma into our workflow, I worked closely with stakeholders across the organization—management, marketing, and front-end development—to ensure that every voice was heard and every need was met. By keeping the lines of <span>communication open and transparent</span>, we were able to move fast from concept to final product, ensuring that the design, functionality, and business objectives were perfectly aligned.
+                                In a fast-paced environment where collaboration was crucial, my ability to communicate effectively and adapt to cross-functional goals became essential in driving success. When it came time to integrate Figma into our workflow, <span>I worked closely with stakeholders</span> across the organization—management, marketing, and front-end development—to ensure that every voice was heard and every need was met. By keeping the lines of <span>communication open and transparent</span>, we were able to move fast from concept to final product, ensuring that the design, functionality, and business objectives were perfectly aligned.
                             </div>
                         </div>
 
@@ -80,7 +80,7 @@ export function About() {
                                 Crafting Impactful Designs That Drive <span>Business Success</span>
                             </div>
                             <div className="paratext">
-                                What excites me most about the opportunity to work with you is the chance to bring the same meaningful impact to your organization. With a proven ability to build scalable design systems, I’m confident my passion for user-centric design and focus on aligning with business goals will exceed your expectations. I’m eager to join a forward-thinking company where design drives success. <span className="lastt">Together, we’ll create beautiful, functional products that deliver measurable results and elevate the business.</span>
+                                What excites me most about the opportunity to work with you is the chance to bring the same <span>meaningful impact to your organization</span>. With a proven ability to build scalable design systems, I’m confident my passion for <span>user-centric design</span> and focus on aligning with <span>business goals</span> will exceed your expectations. I’m eager to join a forward-thinking company where design drives success. <span className="lastt">Together, we’ll create beautiful, functional products that deliver measurable results and elevate the business.</span>
                             </div>
                         </div>
 
